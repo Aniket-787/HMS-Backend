d5bc528dcd336b38ce71aca2f06057de4cceb7e8
Aniket Wagh <aniketwagh878@gmail.com>
2026-04-12 11:31:18 +0530

    fix cors issue

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "https://hms-frontend-black.vercel.app/",
+  origin: "https://hms-frontend-black.vercel.app",
   credentials: true
 }));
 
