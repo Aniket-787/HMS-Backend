a4ee2cecad65ad755b793ae9c5e7fb2cc7996ab3
Aniket Wagh <aniketwagh878@gmail.com>
2026-04-30 07:48:57 +0530

    email service modiofied

--- src/services/email.js
+++ src/services/email.js
@@ -1,14 +1,19 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
-    type: 'OAuth2',
+    type: "OAuth2",
     user: process.env.EMAIL_USER,
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    refreshToken: process.env.REFRESH_TOKEN,
+    refreshToken: process.env.REFRESH_TOKEN
   },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000
 });
 
 // Verify the connection configuration
